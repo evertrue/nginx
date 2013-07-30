58608c5af086108eca6a7cdcf28d70a9b41198eb
Eric Herot <eric.github@herot.com>
2013-07-30 13:47:28 -0400

    Added support for 'socketproxy'

--- attributes/default.rb
+++ attributes/default.rb
@@ -26,6 +26,7 @@
 # default['nginx']['source']['checksum']
 default['nginx']['version'] = "1.2.6"
 default['nginx']['package_name'] = "nginx"
+default['nginx']['port'] = "8080"
 default['nginx']['dir'] = "/etc/nginx"
 default['nginx']['log_dir'] = "/var/log/nginx"
 default['nginx']['binary'] = "/usr/sbin/nginx"
@@ -87,3 +88,5 @@ default['nginx']['types_hash_bucket_size'] = 64
 
 default['nginx']['proxy_read_timeout'] = nil
 default['nginx']['client_max_body_size'] = nil
+
+default['nginx']['default']['modules'] = []
--- recipes/default.rb
+++ recipes/default.rb
@@ -50,3 +50,7 @@ service 'nginx' do
   supports :status => true, :restart => true, :reload => true
   action :start
 end
+
+node['nginx']['default']['modules'].each do |ngx_module|
+  include_recipe "nginx::#{ngx_module}"
+end
