0d7bc403bd6cf3c92010dd4caacdb5ce46923293
Eric Herot <eric.github@herot.com>
2014-09-30 23:20:09 -0400

    Notify existing nginx start resource instead of defining a duplicate resource

--- recipes/default.rb
+++ recipes/default.rb
@@ -21,7 +21,10 @@
 
 include_recipe "et_nginx::#{node['nginx']['install_method']}"
 
-resources(:service => 'nginx').action :start
+ruby_block 'start nginx' do
+  block {}
+  notifies :start, 'service[nginx]'
+end
 
 node['nginx']['default']['modules'].each do |ngx_module|
   include_recipe "et_nginx::#{ngx_module}"
