a74fa375f9a3e500524947bc36b6c299ce4ca4fb
Eric Herot <eric.github@herot.com>
2015-01-15 17:39:33 -0500

    Test should check for port 8080 (not 80)

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -1,12 +1,20 @@
 require 'spec_helper'
 
 describe 'Nginx' do
+  describe file('/tmp/nginx_log') do
+    it { is_expected.to be_directory }
+  end
+
+  describe file('/var/log/nginx') do
+    it { is_expected.to be_symlink }
+  end
+
   describe service 'nginx' do
     it { should be_running }
     it { should be_enabled }
   end
 
-  describe port '80' do
+  describe port '8080' do
     it { should be_listening }
   end
 end
