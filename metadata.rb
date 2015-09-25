2fa4613045de05ffc82ec3f748ef526ae9f611a4
Eric Herot <eric.github@herot.com>
2015-09-25 15:26:04 -0400

    metadata whitespace fix

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,8 @@ license           'Apache 2.0'
 description       'Installs and configures nginx'
 version           '4.0.0'
 
-recipe 'nginx', 'Installs nginx package and sets up configuration with Debian apache style with sites-enabled/sites-available'
+recipe 'nginx', 'Installs nginx package and sets up configuration with ' \
+                'Debian apache style with sites-enabled/sites-available'
 
 depends 'apt',             '~> 2.2'
 depends 'ohai',            '~> 2.0'
