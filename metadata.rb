7ce9b2ad5868c729006449a2443e25693db75b7b
Doug Youch <dougyouch@gmail.com>
2020-02-12 15:07:33 -0500

    removed version lock for apt

--- metadata.rb
+++ metadata.rb
@@ -3,13 +3,13 @@ maintainer        'EverTrue, Inc.'
 maintainer_email  'devops@evertrue.com'
 license           'Apache 2.0'
 description       'Installs and configures nginx'
-version           '6.1.1'
+version           '6.1.2'
 
 recipe 'nginx', 'Installs nginx package and sets up configuration with ' \
                 'Debian apache style with sites-enabled/sites-available'
 
-depends 'apt',             '~> 2.2'
-depends 'ohai',            '~> 4.1'
+depends 'apt'
+depends 'ohai'
 
 supports 'debian'
 supports 'ubuntu'
